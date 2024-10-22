75c184ad63fb1bc63c9dfb2fa6fd6e63b89fbaff
John Martin <john.itvn@gmail.com>
2024-10-22 05:33:41 +0000

    feat(nest-grpc-logger): add nestjs grpc logger library

---
+++ components/libraries/nest-grpc-logger/README.md
@@ -0,0 +1,13 @@
+# nest-grpc-logger
+
+## Lint
+
+Run `nx lint nest-grpc-logger` to build the library.
+
+## Running unit tests
+
+Run `nx test nest-grpc-logger` to execute the unit tests via [Jest](https://jestjs.io).
+
+## Building
+
+Run `nx build nest-grpc-logger` to build the library.
---
+++ components/libraries/nest-grpc-logger/jest.config.ts
@@ -0,0 +1,9 @@
+export default {
+  displayName: 'nest-grpc-logger',
+  preset: '../../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../coverage/components/libraries/nest-grpc-logger',
+};
