eed19733630a901fc539f968bdd34773043b2f83
John Martin <john.itvn@gmail.com>
2024-10-22 05:45:44 +0000

    refactor(common-translator-marker): rename translator-marker project to common-translator-marker

---
+++ components/libraries/common-translator-marker/README.md
@@ -0,0 +1,13 @@
+# common-translator-marker
+
+## Lint
+
+Run `nx lint common-translator-marker` to build the library.
+
+## Running unit tests
+
+Run `nx test common-translator-marker` to execute the unit tests via [Jest](https://jestjs.io).
+
+## Building
+
+Run `nx build common-translator-marker` to build the library.
--- components/libraries/common-translator-marker/jest.config.ts
+++ components/libraries/common-translator-marker/jest.config.ts
@@ -1,9 +1,9 @@
 export default {
-  displayName: 'translator-marker',
+  displayName: 'common-translator-marker',
   preset: '../../../jest.preset.js',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../../coverage/components/libraries/translator-marker',
+  coverageDirectory: '../../../coverage/components/libraries/common-translator-marker',
 };
