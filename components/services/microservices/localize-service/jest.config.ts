acf2525d192286ce7ad0552388ffc0d88ac6f33c
John Martin <john.itvn@gmail.com>
2024-10-24 04:17:06 +0000

    chore: initialize protobuf projects

---
+++ components/services/microservices/localize-service/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'localize-service',
+  preset: '../../../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../../coverage/components/services/microservices/localize-service',
+};
