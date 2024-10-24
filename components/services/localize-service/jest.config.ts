f496df61220eb8aefa2901976b0860bed82f00f8
John Martin <john.itvn@gmail.com>
2024-10-24 10:48:40 +0000

    refactor: move project localize-service

--- components/services/localize-service/jest.config.ts
+++ components/services/localize-service/jest.config.ts
@@ -1,11 +1,11 @@
 export default {
   displayName: 'localize-service',
-  preset: '../../../../jest.preset.js',
+  preset: '../../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../../../coverage/components/services/microservices/localize-service',
+  coverageDirectory: '../../../coverage/components/services/localize-service',
   coveragePathIgnorePatterns: ['/protobuf/'],
 };
