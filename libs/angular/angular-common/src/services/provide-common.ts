12f3b64d6dc1708158d8ed5d111dc44fd667a1b1
John Martin <john.itvn@gmail.com>
2025-01-20 11:37:22 +0000

    chore: update favicon

--- libs/angular/angular-common/src/services/provide-common.ts
+++ libs/angular/angular-common/src/services/provide-common.ts
@@ -1,5 +1,11 @@
 import { HTTP_INTERCEPTORS, provideHttpClient, withFetch } from '@angular/common/http';
-import { EnvironmentProviders, Provider, provideZoneChangeDetection } from '@angular/core';
+import {
+  EnvironmentProviders,
+  inject,
+  provideAppInitializer,
+  Provider,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import {
   provideClientHydration,
   withEventReplay,
@@ -7,6 +13,7 @@ import {
 } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter, Route } from '@angular/router';
+import { WA_WINDOW } from '@ng-web-apis/common';
 import { TUI_ICON_RESOLVER } from '@taiga-ui/core';
 import { NG_EVENT_PLUGINS } from '@taiga-ui/event-plugins';
 import { SystemUrl } from '.';
@@ -20,6 +27,19 @@ export const provideCommon = (appRoutes: Route[]) =>
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
     NG_EVENT_PLUGINS,
+    provideAppInitializer(async () => {
+      const { AssetsBaseURL } = inject(SystemUrl);
+      const { document } = inject(WA_WINDOW);
+      console.log(AssetsBaseURL);
+      if (!document.getElementById('favicon')) {
+        const link = document.createElement('link');
+        link.id = 'favicon';
+        link.type = 'image/x-icon';
+        link.rel = 'icon';
+        link.href = `${AssetsBaseURL}/favicon.ico`;
+        document.getElementsByTagName('head')[0].appendChild(link);
+      }
+    }),
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthenticateInterceptor,
