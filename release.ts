7855539292d1fad3d5bbf6a42ca9432f8f608deb
John Martin <john.itvn@gmail.com>
2025-02-18 16:24:34 +0000

    fix: use release-it 168

---
+++ release.ts
@@ -0,0 +1,21 @@
+import type { Config } from 'release-it';
+
+export default {
+  git: {
+    'changelog': 'npx auto-changelog --stdout --commit-limit false -u --template https://raw.githubusercontent.com/release-it/release-it/main/templates/changelog-compact.hbs',
+    'commit': true,
+    'commitMessage': 'chore: release the v${version}',
+    'addUntrackedFiles': true,
+    'tag': true,
+    'push': true
+  },
+  github: {
+    'release': true,
+    'releaseNotes': 'npx auto-changelog --stdout --commit-limit false -u --template https://raw.githubusercontent.com/release-it/release-it/main/templates/changelog-compact.hbs'
+  },
+  hooks: {
+    'after:bump': [
+      'SHORT_SHA=$(git rev-parse --short HEAD) && yq e ".git-sha = "${SHORT_SHA}"" -i deploy/staging/values.yaml'
+    ]
+  }
+} satisfies Config;
